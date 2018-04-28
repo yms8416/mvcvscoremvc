334efaf8cc84123c76cc916ca28c88f39573c75a
Can PERK <bayms8416@gmail.com>
2018-04-28 11:02:23 +0300

    ef for .net framework

---
+++ BilgeAdam.Web/BilgeAdam.Northwind.NetData/Context/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using BilgeAdam.Northwind.NetData.Entities;
+using System.Data.Entity;
+
+namespace BilgeAdam.Northwind.NetData.Context
+{
+    class NorthwindContext : DbContext
+    {
+        public NorthwindContext() : base("nthCnnStr")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
